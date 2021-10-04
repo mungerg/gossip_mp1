f5a0b0db0d5c2463fae058f64fa9f3d63030b71e
Gabriella Munger <90423480+mungerg@users.noreply.github.com>
2021-10-04 14:32:43 -0400

    created askInput() function and node data structure

---
+++ main.go
@@ -0,0 +1,41 @@
+package gossip_mp1
+
+import "fmt"
+
+type node struct {
+	id     int     // holds ID of node
+	status boolean // false corresponds to susceptible, true corresponds to infected
+	msg    string  // holds message
+}
+
+func main() {
+	message, code := askInput()
+
+}
+
+// asks for user input of web addresses
+func askInput() (string, string, string) {
+	fmt.Println("Hello! Type a word that you would like to send")
+	var message string
+	fmt.Scanln(&message)
+
+	fmt.Println("Enter the letter corresponding to the desired gossip algorithm:")
+	fmt.Println("a. Push")
+	fmt.Println("b. Pull")
+	fmt.Println("c. Push-Pull")
+	var code string
+	fmt.Scanln(&code)
+
+	var printCode string
+	if code == "a" {
+		printCode = "Push"
+	} else if code == "b" {
+		printCode = "Pull"
+	} else {
+		printCode = "Push-Pull"
+	}
+
+	fmt.Println("Great! We will send" + message + "using the" + printCode + "algorithm.")
+
+	return message, code
+}
