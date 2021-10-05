069f4146db2be8e23ae682759962c3dea7e6b7ec
SebastianFernandez1999 <48099921+SebastianFernandez1999@users.noreply.github.com>
2021-10-04 22:40:43 -0400

    Start Gossip & Push Functions

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package gossip_mp1
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"sync"
+	"time"
+	"math/rand"
+)
+
+var listOfNodes []node
 
 type node struct {
 	id     int     // holds ID of node
-	status boolean // false corresponds to susceptible, true corresponds to infected
+	status bool // false corresponds to susceptible, true corresponds to infected
 	msg    string  // holds message
 }
 
 func main() {
 	message, code := askInput()
-	//Hi
+
 }
 
 // asks for user input of web addresses
@@ -38,6 +48,36 @@ func askInput() (string, string, string) {
 	fmt.Println("Great! We will send" + message + "using the" + printCode + "algorithm.")
 
 	return message, code
+}
+func gossip( nodeChan chan <-string,  allInfected <-chan bool, primeNode node, protocol string, message string){
+	lenOfList  := len(listOfNodes)
+	for allInfected != true{
+		//WAIT TIME
+
+		//Pick a random node-This will probably need to be changed according to the protocol developed in chapter 4 that I havent read yet
+		receiver := pickNode(primeNode, lenOfList)
+		if protocol == "Push" && primeNode.status==true {
+			push(receiver, nodeChan, message)
+		}
+		//Need something to do the updating for the receiver. Like right now you could just change the status of the receiver right here
+
+	}
+
+}
+func pickNode(primeNode node, lenOfList int) node {
+	var randomNode int
+	x := false
+	for x == false {
+		randomNode :=rand.Intn(lenOfList)
+		if randomNode != primeNode.id {
+			x = true
+		}
+	}
+	receiver := listOfNodes[randomNode]
+	return receiver
+}
+func push(receiver node, nodeChan chan <-string, message string){
+
 }
 
 
